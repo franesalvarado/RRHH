67ee05bcdb21925eda0387658f9bd342871af52e
Francisco Andrés Alvarado <fran_1848@hotmail.com>
2018-02-02 15:35:11 -0300

    Alertas fin primera etapa

--- src/app/alertas/alertas.component.ts
+++ src/app/alertas/alertas.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpleadoService } from '../servicios/empleado.service';
+import { Empleado } from '../models/empleados';
+import { MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-alertas',
   templateUrl: './alertas.component.html',
-  styleUrls: ['./alertas.component.css']
+  styleUrls: ['./alertas.component.css'],
+  providers: [ EmpleadoService ]
 })
 export class AlertasComponent implements OnInit {
+  public parametro = null;
+  displayedColumns = ['position', 'name', 'legajo', 'tipoContrato'];
+  dataSource;
+  direccion = 'Direccion de Gestion Informatizada';
+  ELEMENT_DATA_SELECT = [];
+  empleados;
+  dialogResult;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private _empleadoService: EmpleadoService
+  ) { }
 
   ngOnInit() {
+    this.empleados = this._empleadoService.getEmpleados();
+    console.log(this.empleados);
+    for (let i in this.empleados){
+      if (this.empleados[i].cantFaltasMensual > 2){
+        this.ELEMENT_DATA_SELECT.push(this.empleados[i]);
+      }
+    }
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA_SELECT);
   }
-
 }
