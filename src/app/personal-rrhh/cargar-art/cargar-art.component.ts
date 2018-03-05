8f9fc3c3cddcfd7abbec8e004d834857787eada4
Francisco Andrés Alvarado <fran_1848@hotmail.com>
2018-03-05 14:00:19 -0300

    Correciones de diseño + cambios User RRHH

--- src/app/personal-rrhh/cargar-art/cargar-art.component.ts
+++ src/app/personal-rrhh/cargar-art/cargar-art.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { Empleado } from '../../libreria/models/empleados';
+import { DataSource } from '@angular/cdk/collections';
+import { EmpleadoService } from '../../libreria/servicios/empleado.service';
 @Component({
   selector: 'app-cargar-art',
   templateUrl: './cargar-art.component.html',
   styleUrls: ['./cargar-art.component.css']
 })
 export class CargarArtComponent implements OnInit {
+  dialogResult;
+  displayedColumns = ['position', 'name', 'legajo', 'tipoContrato'];
+  dataSource;
+  empleados;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private _empleadoService: EmpleadoService) { }
 
   ngOnInit() {
+    this.empleados = this._empleadoService.getEmpleados();
+    this.dataSource = new MatTableDataSource(this.empleados);
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
 
 }
