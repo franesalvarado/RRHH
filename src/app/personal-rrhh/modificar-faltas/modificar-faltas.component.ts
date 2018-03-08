8471f95fa77ebe8cc15de5a87811d75668e2123b
Francisco Andrés Alvarado <fran_1848@hotmail.com>
2018-03-08 15:53:24 -0300

    Implementeacion de menues rrhh

--- src/app/personal-rrhh/modificar-faltas/modificar-faltas.component.ts
+++ src/app/personal-rrhh/modificar-faltas/modificar-faltas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { Empleado } from '../../libreria/models/empleados';
+import { DataSource } from '@angular/cdk/collections';
+import { EmpleadoService } from '../../libreria/servicios/empleado.service';
 
 @Component({
   selector: 'app-modificar-faltas',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar-faltas.component.css']
 })
 export class ModificarFaltasComponent implements OnInit {
+  dialogResult;
+  displayedColumns = ['position', 'name', 'legajo', 'tipoContrato'];
+  dataSource;
+  empleados;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private _empleadoService: EmpleadoService) { }
 
   ngOnInit() {
+    this.empleados = this._empleadoService.getEmpleados();
+    this.dataSource = new MatTableDataSource(this.empleados);
   }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
 
 }
